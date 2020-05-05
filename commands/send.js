6d5cc068a59810e8c45716ee4031339d8ad101f0
SMcMann <57707915+SMcMann@users.noreply.github.com>
2020-05-04 19:11:21 -0700

    Update send.js
    now with comments

--- commands/send.js
+++ commands/send.js
@@ -14,12 +14,13 @@ module.exports = {
         let senderClass = functions.serachID(message.member);
         const user = message.member.displayName        
         var recieverClass;
-
+        
+        //grabs the first mention in a command, otherwise should be undefined
         let x = message.mentions.members.first();
         if (x)
-            recieverClass = functions.searchIDProto(x)//grabs the first mention in a command, otherwise should be undefined
+            recieverClass = functions.searchIDProto(x)
 
-            
+        //checks each arguement provided, and if it is a valid resource accepts it. Otherwise break off throwing error
         var resource= functions.protoValidateResource(args[0]);   
         if (!resource){
             resource = functions.protoValidateResource(args[1])
@@ -31,6 +32,7 @@ module.exports = {
             throw "No vailid resource found"
         }
 
+        //checks each arguement provided, and if there is a valid target accepts it. Otherwise break off throwing error
         if (!recieverClass){
             recieverClass = functions.protoValidateTarget(args[0]);
         }
@@ -44,9 +46,11 @@ module.exports = {
             throw "No vailid target found"
         }
 
+        //stops user from trying to send themselves their own resources
         if (recieverClass === senderClass)
             throw "You cannot send resources to yourself!"
 
+        //checks all arguements to see if there is a valid int provided, then saves it in "amount"
         var amount;
         if (Number.isInteger(parseInt(args[0])))
             amount = args[0];
@@ -57,9 +61,11 @@ module.exports = {
         if (!amount)    
             throw "You did not provide a valid amount"
 
+        //no negative or 0 amounts allowed (no robbery)
         if (amount <= 0)
             throw "Must enter amount greater than 0"
 
+        //lastly checks to see if sender has the correct amount of resources
         let valid = senderClass.checkAmount(amount, resource);
         if (!valid)
             throw ("You do not have enough " + resource + " to send");
@@ -77,24 +83,26 @@ module.exports = {
         message.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👍' || reaction.emoji.name == '👎'),
                 { max: 1, time: 10000 }).then(collected => {
                         if (collected.first().emoji.name == '👍') {
-                                
+                             //the actual exchange of resources   
                             senderClass.outgoing(parseInt(amount), resource); // 
                             recieverClass.incoming(parseInt(amount), resource);
                             //reply to sender confirmation
                             message.reply("Sent " + amount + " " + resource + " to " + recieverClass.fName);
 
+                            //update whoever recieved the resource in their log channel 
                             var channel = message.client.channels.cache.get(recieverClass.logID);
                             channel.send(
                                 user + ` sent you: ` + amount + " " + resource
                             );
-                            //UPDATE BANK INFO FOR PLAYER
+                            //UPDATE BANK INFO FOR RECIEVER
                             channel = message.client.channels.cache.get(recieverClass.bankID);
                             channel.bulkDelete(2);
 
                             var exampleEmbed = functions.newEmbed(recieverClass);
 
+                            //Update sender bank
                             channel.send({ embed: exampleEmbed });
-
+                            
                             channel = message.client.channels.cache.get(senderClass.bankID);
                             channel.bulkDelete(2);
 
